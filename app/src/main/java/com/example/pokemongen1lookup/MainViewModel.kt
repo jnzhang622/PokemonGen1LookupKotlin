87d10e8e47ef234bf2e41100f737f5a6e1299c86
John Zang <jnzhang622@gmail.com>
2021-06-02 13:34:20 -0400

    swapped rxjava for coroutines

--- app/src/main/java/com/example/pokemongen1lookup/MainViewModel.kt
+++ app/src/main/java/com/example/pokemongen1lookup/MainViewModel.kt
@@ -1,16 +1,12 @@
 package com.example.pokemongen1lookup
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokemongen1lookup.data.api.models.PokemonGen1ResponseDTO
 import com.example.pokemongen1lookup.data.api.models.singlepokemonmodels.SinglePokemonResponseDTO
 import com.example.pokemongen1lookup.repos.PokemonRepo
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
@@ -22,11 +18,9 @@ class MainViewModel : ViewModel() {
 
     private var _pokemonGen1Info = MutableLiveData<PokemonGen1ResponseDTO>()
     val pokemonGen1Info get() = _pokemonGen1Info
-//    val pokemonGen1Info : LiveData<PokemonGen1ResponseDTO> get() = _pokemonGen1Info
 
     private var _singlePokemonInfo = MutableLiveData<SinglePokemonResponseDTO>()
     val singlePokemonInfo get() = _singlePokemonInfo
-//    val singlePokemonInfo : LiveData<SinglePokemonResponseDTO> get() = _singlePokemonInfo
 
     init{
         getPokemonGen1Data()
@@ -43,15 +37,7 @@ class MainViewModel : ViewModel() {
         }
     }
 
-//    private fun getPokemonGen1Data() =
-//        disposable.add(
-//            pokemonRepo.getGen1Pokemon().subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(this::onGetPokemonSuccess, this::onGetPokemonError)
-//        )
-
     var singlePokemonName = ""
-
     fun getSinglePokemonData(){
         viewModelScope.launch {
             try {
@@ -62,20 +48,14 @@ class MainViewModel : ViewModel() {
             }
         }
     }
-//    fun getSinglePokemonData() =
-//        disposable.add(
-//            pokemonRepo.getSinglePokemon(singlePokemonName).subscribeOn(Schedulers.io()) // rxJava - creates a background thread
-//                .observeOn(AndroidSchedulers.mainThread()) // rxJava - updating mainthread
-//                .subscribe(this::onGetSinglePokemonSuccess, this::onGetPokemonError)
-//        )
 
-    private fun onGetPokemonSuccess(pokemonInfo: PokemonGen1ResponseDTO) {
-        _pokemonGen1Info.value = pokemonInfo
-    }
-
-    private fun onGetSinglePokemonSuccess(pokemonInfo: SinglePokemonResponseDTO) {
-        _singlePokemonInfo.value = pokemonInfo
-    }
+//    private fun onGetPokemonSuccess(pokemonInfo: PokemonGen1ResponseDTO) {
+//        _pokemonGen1Info.value = pokemonInfo
+//    }
+//
+//    private fun onGetSinglePokemonSuccess(pokemonInfo: SinglePokemonResponseDTO) {
+//        _singlePokemonInfo.value = pokemonInfo
+//    }
 
     private fun onGetPokemonError(e: Throwable) {
         e.message.let { Log.d(TAG, it.toString()) }
